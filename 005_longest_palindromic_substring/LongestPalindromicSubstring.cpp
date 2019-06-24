85db6cf6e9059baee9d5fd2139cfafbcbaf0b0e2
Sophimp <songfangxi2012@163.com>
2019-06-24 18:03:18 +0800

    vim setting for cpp test

--- 005_longest_palindromic_substring/LongestPalindromicSubstring.cpp
+++ 005_longest_palindromic_substring/LongestPalindromicSubstring.cpp
@@ -2,30 +2,30 @@
 #include <stdio.h>
 
 string LongestPalindromicSubstring::longestPalindrome(string s){
-        if(s.empty()) return "";
-        if(s.size() <3) return s;
-        int len = s.size(), start=0, curLen = len;
-        bool find = false;
-        while(curLen > 1){
-            for(start = 0; start + curLen < len; start++){
-                int i=start, j=start+curLen;
-                while(i<j){
-                    if(s[i] == s[j]){
-                        i++;
-                        j--;
-                    }else{
-                        break;
-                    }
-                }
-                if(i >= j){
-                    find = true;
+    if(s.empty()) return "";
+    if(s.size() <3) return s;
+    int len   = s.size(), start=0, curLen = len;
+    bool find = false;
+    while(curLen > 1){
+        for(start = 0; start + curLen < len; start++){
+            int i=start, j=start+curLen;
+            while(i<j){
+                if(s[i] == s[j]){
+                    i++;
+                    j--;
+                }else{
                     break;
                 }
             }
-            if(find) break;
-            curLen--;
+            if(i >= j){
+                find = true;
+                break;
+            }
         }
-        return s.substr(start,start + curLen);
+        if(find) break;
+        curLen--;
+    }
+    return s.substr(start,start + curLen);
 }
 
 int main(string args){
